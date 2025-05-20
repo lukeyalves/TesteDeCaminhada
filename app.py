d64dbcecdb703852faa490f911f55b154006be20
lucas <lucas2010silva@hotmail.com>
2025-05-19 21:54:48 -0300

    consertando2

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import os
 app = Flask(__name__)
 app.secret_key = 'teste6minutos'
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -51,3 +46,9 @@ def index():
                                percentual=round(percentual, 1),
                                classificacao=classificacao)
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
