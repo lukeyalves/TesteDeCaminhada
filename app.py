0669ef289cd15dfa61b8497feb1be26b5cbbb2ad
lucas <lucas2010silva@hotmail.com>
2025-05-19 21:34:36 -0300

    adicionei os

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
